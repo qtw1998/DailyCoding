511a8bdbd68d86f3266cb19c9db31051850f7d84
qtw1998 <qtw1998@163.com>
2020-05-01 21:05:55 +0800

    update BinarySearch.binarySearch

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -7,10 +7,14 @@ public class BinarySearch {
         }
 
         int start = 0, end = nums.length - 1;
+        // nums = [1, 1], target = 1
+        // start = 0, end = 1
+        // nums = [1, 2], target = 1
         while(start < end) {
+            // (0 + 1) / 2 = 0
             int mid = start + (end - start) / 2;
             if(nums[mid] == target) {
-                start = mid;
+                start = mid; // start = mid = 0 dead loop
             } else if (nums[mid] > target) {
                 end = mid - 1;
             } else {
