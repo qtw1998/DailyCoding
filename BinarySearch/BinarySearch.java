259c01d364ddbe80b33dec061268254bd02ef6f9
qtw1998 <qtw1998@163.com>
2020-05-01 21:21:42 +0800

    update BinarySearch.binarySearch

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,7 +1,12 @@
 package BinarySearch;
 
 public class BinarySearch {
-    public int binarySearch(int[] nums, int target) {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4};
+        System.out.println(binarySearch(array, 2));
+    }
+    public static int binarySearch(int[] nums, int target) {
         if(nums == null || nums.length == 0) {
             return -1;
         }
@@ -10,7 +15,7 @@ public class BinarySearch {
         // nums = [1, 1], target = 1
         // start = 0, end = 1
         // nums = [1, 2], target = 1
-        while(start < end) {
+        while(start < end) { // == break [0, 1]  ×
             // (0 + 1) / 2 = 0
             int mid = start + (end - start) / 2;
             if(nums[mid] == target) {
